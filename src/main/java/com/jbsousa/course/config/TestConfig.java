1f4282b723ae61c56af97f6fae0429cc4dad0f43
João Barbosa <joao.2010pm@gmail.com>
2021-10-12 20:53:30 -0300

    Payment, one-to-one association

--- src/main/java/com/jbsousa/course/config/TestConfig.java
+++ src/main/java/com/jbsousa/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jbsousa.course.entities.Category;
 import com.jbsousa.course.entities.Order;
 import com.jbsousa.course.entities.OrderItem;
+import com.jbsousa.course.entities.Payment;
 import com.jbsousa.course.entities.Product;
 import com.jbsousa.course.entities.User;
 import com.jbsousa.course.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
